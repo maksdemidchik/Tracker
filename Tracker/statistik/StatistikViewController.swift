fe789dd5e859341f710a9abdf36581a2b7b32030
maksdemidchik <maksimdemidcik37@gmail.com>
2025-05-03 14:15:24 +0300

    sprint_14

--- Tracker/statistik/StatistikViewController.swift
+++ Tracker/statistik/StatistikViewController.swift
@@ -12,18 +12,7 @@ final class StatistikViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .whiteYP
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
